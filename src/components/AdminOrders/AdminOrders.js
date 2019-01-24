e07484e1677a9fe1de358694e0543350f1121ae7
Maxwell Todd <mtmaxtodd@gmail.com>
2019-01-24 14:55:32 -0600

    Add JSON.stringify() to AdminOrders.js.

--- src/components/AdminOrders/AdminOrders.js
+++ src/components/AdminOrders/AdminOrders.js
@@ -1,13 +1,48 @@
+import axios from 'axios';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class AdminOrders extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            orders: [],
+        };
+    }
+
+    // Runs after the page loads
+    componentDidMount() {
+        this.getOrders();
+    }
+
+    // Request all previous orders from the server (via route /api/order GET)
+    getOrders = () => {
+        console.log('getOrders()');
+        axios.get('/api/order', (response) => {
+            console.log(response);
+            this.setState({
+                orders: response,
+            });
+        }).catch((error) => {
+            const errorMessage = `Server Error: ${error}`;
+            alert(errorMessage);
+            console.log(errorMessage);
+        })
+    }
+
+    // Display this component
     render() {
         return (
             <div>
+                {JSON.stringify(this.state.orders)}
                 <p>[AdminOrders]</p>
             </div>
         );
     }
 }
 
-export default AdminOrders;
+const mapReduxStoreToProps = (reduxStore) => ({
+    reduxStore: reduxStore
+});
+
+export default connect(mapReduxStoreToProps)(AdminOrders);
