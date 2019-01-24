1f95ab63f80373b04b251b3166bf023d5d069867
Anthony D <dunahee@gmail.com>
2019-01-24 13:18:15 -0600

    hash router set up, route and links set up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import OrderCustomerInfo from './../OrderCustomerInfo/OrderCustomerInfo.js';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+
+//Remeber to import your components
 
 class App extends Component {
   render() {
@@ -11,10 +15,28 @@ class App extends Component {
         </header>
 
         {/* HasRouter */}
+        <Router>
+          <div>
+            {/* write in your order select pizza link */}
 
-        {/* Nav Links */}
+            {/* write in your order customer info link */}
+            <Link to="/order-customer-info">Order Customer Info</Link>
+            {/* write in your order checkout link */}
 
-        {/* Routes */}
+            {/* write in your admin order link */}
+
+            {/* Routes */}
+
+            {/* write in your order select pizza route */}
+            <Route exact path="/order-customer-info" component={OrderCustomerInfo} />
+            {/* write in your order customer info route */}
+
+            {/* write in your order checkout route */}
+
+            {/* write in your admin order route */}
+          </div>
+        </Router>
+        
 
         <br/>
         <img src="images/pizza_photo.png"/>
