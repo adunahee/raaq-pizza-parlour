c8c479a72826e9be8ecf6fe701af482cec8780e2
Maxwell Todd <mtmaxtodd@gmail.com>
2019-01-25 08:55:59 -0600

    edited how the time shows in the admin table.

--- src/components/AdminTableRow/AdminTableRow.js
+++ src/components/AdminTableRow/AdminTableRow.js
@@ -6,7 +6,7 @@ class AdminTableRow extends Component {
         return (
             <tr>
                 <td>{order.customer_name}</td>
-                <td>{order.time}</td>
+                <td>{order.time.substr(11, 5)}</td>
                 <td>{order.type}</td>
                 <td>{order.total}</td>
             </tr>
